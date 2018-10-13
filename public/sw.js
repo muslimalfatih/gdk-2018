a07234c2b2795bf995fe419f300d7694ff21b07c
Muslim Al Fatih <muslimalfatih22@gmail.com>
2018-10-13 17:20:50 +0700

    init commit

---
+++ public/sw.js
@@ -0,0 +1,54 @@
+// Install SW
+const staticCachesName = 'static-v9'
+const fileToCaches = [
+  '/index.html',
+  '/calculator/index.html',
+  '/calculator/app.css',
+  '/calculator/app.js',
+  '/images/logo-MF.png',
+  '/images/muslimalfatih-af.jpg',
+  '/mapbox/index.html',
+  '/mapbox/app.css'
+]
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(staticCachesName).then((cache) => {
+      return cache.addAll(fileToCaches)
+    })
+  )
+  // console.log('Service Worker Installin', event)
+})
+
+self.addEventListener('activate', () => {
+  console.log('Active')
+})
+
+self.addEventListener('fetch', (event) => {
+
+  console.log('Fetching:', event.request.url)
+  // console.log(event.request)
+  // console.log(event.request.method)
+  // console.log(event.request.headers)
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if(response) {
+          console.log(`Found ${event.request.url} in cache`)
+          return response;
+        }
+        console.log(`Network request for ${event.request.url}`)
+        return fetch(event.request).then(response => {
+          return caches.open(staticCachesName).then(cache => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        });
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  )
+
+// event.respondWith(response);
+})
+
